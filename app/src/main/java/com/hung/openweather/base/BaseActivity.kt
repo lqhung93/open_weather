37a456e2ffd74352906b387e17f2a715d15422fd
Hung Le <lqhung93@gmail.com>
2022-02-01 23:26:18 +0700

    add base classes

---
+++ app/src/main/java/com/hung/openweather/base/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.hung.openweather.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Base for every Activity in the App.
+ */
+abstract class BaseActivity : AppCompatActivity() {
+
+}
