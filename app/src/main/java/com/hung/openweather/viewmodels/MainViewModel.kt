a2f5904ab9925241c6d0da3246d71093d79c570f
Hung Le <lqhung93@gmail.com>
2022-02-04 14:53:01 +0700

    add main view model

---
+++ app/src/main/java/com/hung/openweather/viewmodels/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.hung.openweather.viewmodels
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hung.openweather.models.WeatherResponse
+import com.hung.openweather.repository.MainRepository
+import io.reactivex.Single
+
+class MainViewModel(private val repository: MainRepository) : ViewModel() {
+
+    fun getDailyForecast(): Single<WeatherResponse> {
+        return repository.getDailyForecast()
+    }
+
+    companion object {
+
+        fun builder(context: Context): ViewModelProvider.Factory {
+            val repository = MainRepository()
+            return MainViewModelFactory(repository)
+        }
+    }
+
+}
