9ed7b035fa77ead0b1833781c4f909f3735b1c34
Hung Le <lqhung93@gmail.com>
2022-02-04 15:07:55 +0700

    add logic to main fragment

--- app/src/main/java/com/hung/openweather/fragments/MainFragment.kt
+++ app/src/main/java/com/hung/openweather/fragments/MainFragment.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.hung.openweather.adapters.WeatherAdapter
 import com.hung.openweather.base.BaseFragment
 import com.hung.openweather.databinding.FragmentMainBinding
+import com.hung.openweather.models.WeatherResponse
+import com.hung.openweather.viewmodels.MainViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class MainFragment : BaseFragment() {
 
+    private lateinit var mainViewModel: MainViewModel
+
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(this, MainViewModel.builder(requireContext())).get(MainViewModel::class.java)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
@@ -19,6 +35,29 @@ class MainFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = WeatherAdapter()
+
+        binding.rvWeatherForecast.addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+        binding.rvWeatherForecast.adapter = adapter
+
+        binding.btnGetWeather.setOnClickListener {
+            mainViewModel.getDailyForecast()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<WeatherResponse>() {
+                    override fun onSuccess(t: WeatherResponse) {
+                        t.list?.let {
+                            adapter.setData(it)
+                        }
+
+                    }
+
+                    override fun onError(e: Throwable) {
+
+                    }
+                })
+        }
     }
 
     override fun onDestroyView() {
