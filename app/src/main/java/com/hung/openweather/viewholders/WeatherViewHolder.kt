8f50d720cd259b51ae9c9974dc243d7615a5434a
Hung Le <lqhung93@gmail.com>
2022-02-05 20:20:28 +0700

    change name

--- app/src/main/java/com/hung/openweather/viewholders/WeatherViewHolder.kt
+++ app/src/main/java/com/hung/openweather/viewholders/WeatherViewHolder.kt
@@ -3,11 +3,11 @@ package com.hung.openweather.viewholders
 import androidx.recyclerview.widget.RecyclerView
 import com.hung.openweather.databinding.ListItemWeatherBinding
 import com.hung.openweather.models.WeatherData
-import com.hung.openweather.viewmodels.WeatherViewHolderDataModel
+import com.hung.openweather.viewmodels.WeatherViewHolderViewModel
 
 class WeatherViewHolder(private val binding: ListItemWeatherBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(weatherData: WeatherData) {
-        binding.dataModel = WeatherViewHolderDataModel(weatherData)
+        binding.viewModel = WeatherViewHolderViewModel(weatherData)
     }
 }
