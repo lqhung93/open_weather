0e88233669feb8ad77af14e70819b21867c389b4
Hung Le <lqhung93@gmail.com>
2022-02-05 13:37:11 +0700

    add network data source for load data from API

---
+++ app/src/main/java/com/hung/openweather/data/network/NetworkDataSource.kt
@@ -0,0 +1,15 @@
+package com.hung.openweather.data.network
+
+import com.hung.openweather.App
+import com.hung.openweather.data.DataSource
+import com.hung.openweather.models.WeatherResponse
+import com.hung.openweather.utils.Utils
+import io.reactivex.Observable
+
+class NetworkDataSource : DataSource {
+
+    override fun getDailyForecast(query: String): Observable<WeatherResponse> =
+        App.getApiManger().weatherService
+            .getDailyForecast(Utils.getAppId(), query, "7", "metric")
+
+}
